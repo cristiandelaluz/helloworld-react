3774610054640f627e63b880c86b41d27cb7bd27
Cristian De La Luz <cristhian.oh16@gmail.com>
2019-03-11 13:21:28 +0100

    inline styles

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import './Person.css';
+/* import Radium from 'radium'; */
 
 const person = (props) => {
+  const style = {
+    '@media (min-width: 500px)': {
+      width: '450px'
+    }
+  }
+
   return (
     <div className="Person">
       <h3 onClick={ props.click }>I'm a { props.name } ! and I am { props.age } years old</h3>
